69399d3844298aa13d1426a261045d83b9c3586c
Favian-Ioel Poputa <favianioel@gmail.com>
2019-03-10 22:45:28 +0200

    life

--- src/app.php
+++ src/app.php
@@ -8,5 +8,6 @@ $routes = new RouteCollection();
 
 $routes->add('hello', new Route('/hello/{name}', ['name' => 'World']));
 $routes->add('bye'  , new Route('/bye'));
+$routes->add('this-is-life', new Route('/this-is-life'));
 
 return $routes;
